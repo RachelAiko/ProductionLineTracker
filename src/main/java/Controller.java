5496fb2e70b8166cffff1822b9841d4b58e8627e
Rachel <ramatthews7888@eagle.fgcu.edu>
2020-08-27 15:25:48 -0400

    updated CSS file

--- src/main/java/Controller.java
+++ src/main/java/Controller.java
@@ -1,12 +1,12 @@
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class Controller{
+public class Controller {
     @FXML
     private Label lblOutput;
 
     @FXML
-    private void sayHello(){
+    private void sayHello() {
         lblOutput.setText("Hello!");
     }
 
