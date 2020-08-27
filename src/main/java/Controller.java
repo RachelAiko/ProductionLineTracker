e5746277a5f2db2e76d94c4161e6fbc7f8694bb7
Rachel <ramatthews7888@eagle.fgcu.edu>
2020-08-27 15:21:44 -0400

    Initial commit

---
+++ src/main/java/Controller.java
@@ -0,0 +1,13 @@
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class Controller{
+    @FXML
+    private Label lblOutput;
+
+    @FXML
+    private void sayHello(){
+        lblOutput.setText("Hello!");
+    }
+
+}
