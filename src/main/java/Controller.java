c71a5ca039f978a98f21f0d22e0141f4d0daba7a
Rachel <ramatthews7888@eagle.fgcu.edu>
2020-09-03 13:05:14 -0400

    Week 3 Changes

--- src/main/java/Controller.java
+++ src/main/java/Controller.java
@@ -1,7 +1,14 @@
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Controller {
+
+
     @FXML
     private Label lblOutput;
 
@@ -19,4 +26,22 @@ public class Controller {
     private void recordProduction() {
         System.out.println("Production Recorded");
     }
+
+    @FXML
+    private void itemType() {
+    }
+
+    @FXML
+    private void chooseQuantity() {
+        List<String> chooseQuantity = new ArrayList<String>();
+        chooseQuantity.add("1");
+        chooseQuantity.add("2");
+        chooseQuantity.add("3");
+    }
+
+    @FXML
+    private void initialize() {
+
+    }
+
 }
