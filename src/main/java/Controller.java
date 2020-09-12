cab0c9eb41742b659f1b3d6c55cf55292fa4d2ed
Rachel <ramatthews7888@eagle.fgcu.edu>
2020-09-12 14:54:45 -0400

    Week 4 Changes added database

--- src/main/java/Controller.java
+++ src/main/java/Controller.java
@@ -3,8 +3,16 @@ import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 
 public class Controller {
 
@@ -43,5 +51,43 @@ public class Controller {
     private void initialize() {
 
     }
+
+    final String JDBC_DRIVER = "org.h2.Driver";
+    final String DB_URL = "jdbc:h2:./resources/PLdb";
+
+    //  Database credentials
+    final String USER = "";
+    final String PASS = "";
+    Connection conn = null;
+    Statement stmt = null;
+
+       /*try {
+        // STEP 1: Register JDBC driver
+        Class.forName(JDBC_DRIVER);
+
+        //STEP 2: Open a connection
+        //conn = DriverManager.getConnection(DB_URL, USER, PASS);
+        conn = DriverManager.getConnection(DB_URL);
+
+
+        //STEP 3: Execute a query
+        stmt = conn.createStatement();
+
+
+        ResultSet rs = stmt.executeQuery(sql);
+        while (rs.next()) {
+            System.out.println(rs.getString(1));
+        }
+
+        // STEP 4: Clean-up environment
+        stmt.close();
+        conn.close();
+    } catch (ClassNotFoundException e) {
+        e.printStackTrace();
+
+    } catch (
+    SQLException e) {
+        e.printStackTrace();
+    }*/
 
 }
