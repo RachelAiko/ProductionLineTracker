7b9a31a9693c92e5d5d2980dd26a7391e36a4291
Rachel <ramatthews7888@eagle.fgcu.edu>
2020-09-15 15:14:48 -0400

    Week 4 Changes fixed database connection

--- src/main/java/Controller.java
+++ src/main/java/Controller.java
@@ -15,83 +15,83 @@ import java.sql.SQLException;
 import java.sql.Statement;
 */
 
-
 public class Controller {
 
+  @FXML
+  private Label lblOutput;
 
-    @FXML
-    private Label lblOutput;
+  @FXML
+  private void sayHello() {
+    lblOutput.setText("Hello!");
+  }
 
-    @FXML
-    private void sayHello() {
-        lblOutput.setText("Hello!");
+  @FXML
+  private void addProduct() {
+    //String sql = "INSERT INTO Product(type, manufacturer, name)" + "VALUES ('AUDIO', " +
+    //  "'Apple', 'iPod')";
+    System.out.println("Product Added");
+  }
+
+  @FXML
+  private void recordProduction() {
+    System.out.println("Production Recorded");
+  }
+
+  @FXML
+  private void itemType() {
+  }
+
+  @FXML
+  private void chooseQuantity() {
+    List<String> chooseQuantity = new ArrayList<String>();
+    chooseQuantity.add("1");
+    chooseQuantity.add("2");
+    chooseQuantity.add("3");
+  }
+
+  @FXML
+  private void initialize() {
+    initializeDB();
+  }
+
+
+  public void initializeDB() {
+    final String JDBC_DRIVER = "org.h2.Driver";
+    final String DB_URL = "jdbc:h2:./resources/PLdb";
+
+    //  Database credentials
+    final String USER = "";
+    final String PASS = "";
+    Connection conn = null;
+    Statement stmt = null;
+
+    try {
+      // STEP 1: Register JDBC driver
+      Class.forName(JDBC_DRIVER);
+
+      //STEP 2: Open a connection
+      conn = DriverManager.getConnection(DB_URL, USER, PASS);
+
+      //STEP 3: Execute a query
+      stmt = conn.createStatement();
+      String sql = "SELECT * FROM Product ";
+      //String sql = "INSERT INTO Product(type, manufacturer, name)" + "VALUES ('AUDIO', " +
+      //"'Apple', 'iPod')";
+
+      ResultSet rs = stmt.executeQuery(sql);
+      while (rs.next()) {
+        System.out.println(rs.getString(1));
+      }
+
+      // STEP 4: Clean-up environment
+      stmt.close();
+      conn.close();
+    } catch (ClassNotFoundException e) {
+      e.printStackTrace();
+
+    } catch (SQLException e) {
+      e.printStackTrace();
     }
-
-    @FXML
-    private void addProduct() {
-        //String sql = "INSERT INTO Product(type, manufacturer, name)" + "VALUES ('AUDIO', " +
-              //  "'Apple', 'iPod')";
-        System.out.println("Product Added");
-    }
-
-    @FXML
-    private void recordProduction() {
-        System.out.println("Production Recorded");
-    }
-
-    @FXML
-    private void itemType() {
-    }
-
-    @FXML
-    private void chooseQuantity() {
-        List<String> chooseQuantity = new ArrayList<String>();
-        chooseQuantity.add("1");
-        chooseQuantity.add("2");
-        chooseQuantity.add("3");
-    }
-
-    @FXML
-    private void initialize() {
-        initializeDB();
-    }
-    public void initializeDB() {
-        final String JDBC_DRIVER = "org.h2.Driver";
-        final String DB_URL = "jdbc:h2:./resources/PLdb";
-
-        //  Database credentials
-        final String USER = "";
-        final String PASS = "";
-        Connection conn = null;
-        Statement stmt = null;
-
-        try {
-            // STEP 1: Register JDBC driver
-            Class.forName(JDBC_DRIVER);
-
-            //STEP 2: Open a connection
-            conn = DriverManager.getConnection(DB_URL, USER, PASS);
-
-            //STEP 3: Execute a query
-            stmt = conn.createStatement();
-            String sql = "SELECT *";
-            //String sql = "INSERT INTO Product(type, manufacturer, name)" + "VALUES ('AUDIO', " +
-                    //"'Apple', 'iPod')";
-
-            ResultSet rs = stmt.executeQuery(sql);
-            while (rs.next()) {
-                System.out.println(rs.getString(1));
-            }
-
-            // STEP 4: Clean-up environment
-            stmt.close();
-            conn.close();
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
+  }
 
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,8 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -6,21 +11,59 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
 
-    public static void main(String[] args) {
-        launch(args);
+  public static void main(String[] args) {
+    launch(args);
+  }
+
+  @Override
+  public void start(Stage primaryStage) throws Exception {
+    Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+
+    Scene scene = new Scene(root, 680, 580);
+    scene.getStylesheets().add
+        (Main.class.getResource("Style.css").toExternalForm());
+
+    primaryStage.setTitle("Production Line Tracker");
+    primaryStage.setScene(scene);
+    primaryStage.show();
+  }
+
+ /* public void initializeDB() {
+    final String JDBC_DRIVER = "org.h2.Driver";
+    final String DB_URL = "jdbc:h2:./resources/PLdb";
+
+    //  Database credentials
+    final String USER = "";
+    final String PASS = "";
+    Connection conn = null;
+    Statement stmt = null;
+
+    try {
+      // STEP 1: Register JDBC driver
+      Class.forName(JDBC_DRIVER);
+
+      //STEP 2: Open a connection
+      conn = DriverManager.getConnection(DB_URL, USER, PASS);
+
+      //STEP 3: Execute a query
+      stmt = conn.createStatement();
+      String sql = "SELECT * FROM product";
+      //String sql = "INSERT INTO Product(type, manufacturer, name)" + "VALUES ('AUDIO', " +
+      //"'Apple', 'iPod')";
+
+      ResultSet rs = stmt.executeQuery(sql);
+      while (rs.next()) {
+        System.out.println(rs.getString(1));
+      }
+
+      // STEP 4: Clean-up environment
+      stmt.close();
+      conn.close();
+    } catch (ClassNotFoundException e) {
+      e.printStackTrace();
+
+    } catch (SQLException e) {
+      e.printStackTrace();
     }
-
-
-    @Override
-    public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-
-        Scene scene = new Scene(root, 680, 580);
-        scene.getStylesheets().add
-                (Main.class.getResource("Style.css").toExternalForm());
-
-        primaryStage.setTitle("Production Line Tracker");
-        primaryStage.setScene(scene);
-        primaryStage.show();
-    }
+  }*/
 }
